6af4779f6704563a8c1beb5b4920a64713da8bb8
Matje1979 <ibnruzd@yahoo.com>
2022-04-05 08:40:04 +0200

    Created a function that returns commission based on date.

---
+++ jpt.py
@@ -0,0 +1,21 @@
+import re
+
+
+def get_commission(date_str):
+    """
+    Function that takes a string in the YYYY-MM-DD format and
+    returns an info about the related commission
+    """
+    # Check if date_str is a valid date.
+    pattern = re.compile(
+        "^[0-2][0-9][0-9][0-9]-((0[1-9])|(1[0-2]))-(0[1-9]|[1-2][0-9]|3[0-1])$"
+    )
+    assert pattern.match(date_str)
+
+    date_list = date_str.split("-")
+    if 6 > int(date_list[1][1]) < 9:
+        return float(15)
+    return float(10)
+
+
+print(get_commission("1999-10-01"))
